97ff87996e7606422ca00774f79504aac74406a0
Pierre Paquin <p.paquin1016@laposte.net>
2023-03-24 16:12:13 +0100

    refactor: clean code

--- src/api/players/components/app_launcher.rs
+++ src/api/players/components/app_launcher.rs
@@ -3,19 +3,19 @@ use crate::api::players::services::player_repository_mongo::PlayerRepositoryMong
 use crate::api::players::routes::read_test::ressources;
 use crate::api::players::routes::player_read_router::get_players;
 use crate::api::players::routes::player_write_router::create_command;
+use crate::core::players::errors::custom::CustomError;
 
 pub struct AppLauncher;
 
 impl AppLauncher {
-    pub async fn launch_rocket() -> Result<Rocket<Build>, Box<dyn std::error::Error>> {
-        match PlayerRepositoryMongo::new().await {
-            Ok(player_repository) => Ok(
+    pub async fn launch_rocket() -> Result<Rocket<Build>, CustomError> {
+        PlayerRepositoryMongo::new().await
+            .map(|player_repository| {
                 rocket::build()
                     .manage(player_repository)
                     .mount("/", routes![ressources, get_players, create_command])
-            ),
-            Err(e) => Err(Box::new(e))
-        }
+            })
+            .map_err(|err| CustomError::new(err.to_string().as_str()))
     }
 }
 
--- src/core/players/errors/custom.rs
+++ src/core/players/errors/custom.rs
@@ -1,20 +1,20 @@
-// use std::fmt::{self, Display, Formatter};
-//
-// #[derive(Debug)]
-// pub struct CustomError {
-//     message: String
-// }
-//
-// impl CustomError {
-//     pub fn new(message: &str) -> Self {
-//         Self {message: message.to_string()}
-//     }
-// }
-//
-// impl Display for CustomError {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-//         write!(f, "Custom error : {}", self.message)
-//     }
-// }
-//
-// impl std::error::Error for CustomError {}
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug)]
+pub struct CustomError {
+    message: String
+}
+
+impl CustomError {
+    pub fn new(message: &str) -> Self {
+        Self {message: message.to_string()}
+    }
+}
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Custom error : {}", self.message)
+    }
+}
+
+impl std::error::Error for CustomError {}
