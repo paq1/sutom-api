ae6d3716812643c23bca76bf2ece69d19a51a795
paq1 <32450977+paq1@users.noreply.github.com>
2023-03-25 11:10:16 +0100

    Ajout derniere routes (#2)
    * wip: ajout recup player by name
    
    * fix: handle wrong status

--- src/api/players/components/app_launcher.rs
+++ src/api/players/components/app_launcher.rs
@@ -2,6 +2,7 @@ use rocket::{Build, Rocket};
 use crate::api::players::services::player_repository_mongo::PlayerRepositoryMongo;
 use crate::api::players::routes::read_test::ressources;
 use crate::api::players::routes::player_read_router::get_players;
+use crate::api::players::routes::player_read_router::get_player_by_name;
 use crate::api::players::routes::player_write_router::create_command;
 use crate::core::players::errors::custom::CustomError;
 
@@ -13,7 +14,15 @@ impl AppLauncher {
             .map(|player_repository| {
                 rocket::build()
                     .manage(player_repository)
-                    .mount("/", routes![ressources, get_players, create_command])
+                    .mount(
+                        "/",
+                        routes![
+                            ressources,
+                            get_players,
+                            get_player_by_name,
+                            create_command
+                        ]
+                    )
             })
             .map_err(|err| CustomError::new(err.to_string().as_str()))
     }
